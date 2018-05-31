33a6227e6b66034797e8f86438873f8232f8cd15
Daishi Kaszer <daishi.kaszer@yahoo.fr>
2018-06-01 00:00:56 +0200

    Improve code

--- public/student-edit.php
+++ public/student-edit.php
@@ -28,7 +28,15 @@ $conn = DriverManager::getConnection($connectionParams, $config);
 $loader = new Twig_Loader_Filesystem(__DIR__.'/../templates');
 $twig = new Twig_Environment($loader);
 
+// affectation de l'id en utilisant l'opérateur ternaire
 $id = !empty($_GET['id']) ? $_GET['id'] : 0;
+
+// l'utilisation de l'opérateur ternaire remplace le bloc de code suivant
+// if (!empty($_GET['id'])) {
+//     $id = $_GET['id'];
+// } else {
+//     $id = 0;
+// }
 
 // envoi d'une requête SQL à la BDD et récupération du premier résultat sous forme de tableau PHP dans la variable `$student`
 $student = $conn->fetchAssoc('SELECT * FROM students WHERE id = :id', [
